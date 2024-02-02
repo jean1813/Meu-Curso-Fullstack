5f2a67a71d7373a1d193412a793d4153f212bcd8
jean <jeansousa.carlos@hotmail.com>
2024-02-02 17:30:32 -0300

    aula 02-02

--- Módulo 02/07 - Interação com API/Aula 01 - Pré-API/Exemplo 01/questao-01.js
+++ Módulo 02/07 - Interação com API/Aula 01 - Pré-API/Exemplo 01/questao-01.js
@@ -14,8 +14,27 @@ function teste(){
 }
 */
 
+/*
 setInterval(acionar, 2000)
 
 function acionar(){
     document.getElementById("demo").textContent += "Estuda"
 }
+*/
+
+/*
+setTimeout(acionar, 3000)  => // comando função // acionar uma função depois de um tempo determinado
+
+function acionar(){
+    alert("GRATIDÃO")
+    alert("PRA QUEM NÃO SABE DE ONDE VEIO QUALQUER CAMINHO É LADO")
+}
+*/
+
+setInterval(teste, 2000)  //=> // executar uma função a cada x tempo
+
+function teste(){
+ 
+   let elemento = document.getElementById("demo")
+   elemento.textContent = elemento.textContent + "  Arthur e Enzo"
+}   
--- Módulo 02/07 - Interação com API/Aula 01 - Pré-API/Exemplo 02/questao-02.js
+++ Módulo 02/07 - Interação com API/Aula 01 - Pré-API/Exemplo 02/questao-02.js
@@ -1,5 +1,4 @@
-
-
+/*
 function teste(x,y){
     //return "oi"
     return x + 5 - y
@@ -7,3 +6,34 @@ function teste(x,y){
 
 let senai = teste(3,4)
 alert(senai)
+*/
+
+function somar(){
+    //return "oi"
+    return 3 + 5
+}
+
+//alert(somar())
+
+/*
+if (somar() > 9){
+    alert("é maior")
+}else{
+    alert("não é maior")
+}
+*/
+/*
+function somar(x,y){
+    return x + y
+}
+alert(somar("ola é ",5))
+*/
+
+
+let num1 = parseInt(prompt("Digite um numero"))
+let num2 = parseInt(prompt("Digite outro numero"))
+
+function somar(num1,num2){
+    return num1 + num2
+}
+alert(somar(num1,num2))
