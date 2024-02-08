257ea6c0a06db0d2a059dc29a846070598197259
jean <jeansousa.carlos@hotmail.com>
2024-02-08 16:56:43 -0300

    usando listas, for, document.body.innerHTML

---
+++ Módulo 02/07 - Interação com API/Aula 04/Exemplo 01/exemplo-01.js
@@ -0,0 +1,42 @@
+async function mexer(){
+    let resposta = await fetch("msg.txt")
+    let convertido = await resposta.text()
+    //console.log(convertido)
+    //alert(convertido)
+
+    //document.body.innerHTML = "<h1>" + convertido + "</h1>"
+
+    //document.body.innerHTML = `<h1>${convertido}</h1>`
+
+    /*document.body.innerHTML = `
+        <h1>
+            ${convertido}
+        </h1>
+        <p>
+            como esta?
+        </p>
+    `*/
+
+    let tnt = [2, 13, 19]
+    for (let x in tnt){  
+        //alert(x) 
+    }
+
+    for (let x = 0; x <= 2; x++) {
+        
+        //alert(x)
+
+        document.body.innerHTML += `
+            <h1>
+                ${convertido}
+            </h1>
+            <p>
+                ${tnt[x]}
+            </p>
+        `
+    }
+}
+
+mexer() 
+
+
