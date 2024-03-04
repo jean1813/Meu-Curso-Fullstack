7b499b713484e9d5f4bc1f976b6b8b6f242a4b21
jean <jeansousa.carlos@hotmail.com>
2024-03-04 17:20:34 -0300

    atividade 01 e 02

---
+++ Módulo 02/07 - Interação com API/Aula 07/Atividade extra 01/ati-01.js
@@ -0,0 +1,25 @@
+async function brasil(){
+    let jogo = await fetch("ati-01.json")
+    let fut = await jogo.json()
+    //console.log(fut)
+
+    let aposta = document.getElementById("lista-card")
+
+    for (let gol of fut) {
+        aposta.innerHTML +=`
+        <div class="card">
+
+        <p>${gol.time}</p>
+        <p>${gol.estado}</p>
+        <p>${gol.estadio}</p>
+        <img src="${gol.img}" width="200" height ="200"> 
+
+        </div>
+        `
+        
+        
+    }
+    
+}
+
+brasil()
