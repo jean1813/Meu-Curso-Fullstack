113c84835ffae7bc193ba59059ac8335f26a8066
jean <jeansousa.carlos@hotmail.com>
2024-03-12 17:25:23 -0300

    mais codigos para o projeto

--- Módulo 02/07 - Interação com API/Aula 06/Exempolo 05/index.js
+++ Módulo 02/07 - Interação com API/Aula 06/Exempolo 05/index.js
@@ -1,27 +1,31 @@
 // Função assíncrona para fazer o uso do Await
 async function buscas(){
-    // Await para a execução do programa aguardando
+    // Await para(stop) a execução do programa aguardando
     // o resultado do comando fetch()
-    let procura = await fetch("lista-produtos.json")
-    let produtos = await procura.json()
-    let divLista = document.getElementById("lista-card")
+    let procura = await fetch("lista-produtos.json")//objeto resposta
+    let produtos = await procura.json()//lista do objeto resposta
+    let divLista = document.getElementById("lista-card")//retorna a tag html div.
     //console.log(produtos)
     //alert(procura)
 
+    // por que usar o for? => pra percorrer uma lista
+    // Quantas linhas de comando tem dentro do for? => 1 linha(divLista.inneHTML)
     for(let produto of produtos){
         divLista.innerHTML += `
             <div class="card">
                 <h3>${produto.nome}</h3>
                 <p>${produto.descricao}</p>
-                <p> valor com desconto R$ ${produto.valorComDesconto}</p>
-                <p> valor sem desconto R$ ${produto.valorSemDesconto}</p>
+                <div class="valores">
+                    <span>R$ ${produto.valorComDesconto.toFixed(2)}</span>
+                    <span>R$ ${produto.valorSemDesconto.toFixed(2)}</span>
+                </div>
             </div>
-
         `
     }
 }
 buscas()
 
+    //in = ao indice da lista(0,1,2...)
     //for(let produto in produtos){
        // divLista.innerHTML += `
             //<div class="card">
