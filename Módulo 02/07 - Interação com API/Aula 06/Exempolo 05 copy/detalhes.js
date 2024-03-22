1c2b1c1875ff9c95540dd2f9c3105ae9e13c8609
jean carlos <jeansousa.carlos@hotmail.com>
2024-03-22 17:00:04 -0300

    Update detalhes.js

--- Módulo 02/07 - Interação com API/Aula 06/Exempolo 05 copy/detalhes.js
+++ Módulo 02/07 - Interação com API/Aula 06/Exempolo 05 copy/detalhes.js
@@ -1,11 +1,34 @@
 async function buscarDetalhes(){
+    // Consumo do JSON
     let busca = await fetch("lista-produtos.json")
     let produtos = await busca.json()
-    
+    console.log(produtos)
+
+    // Criou um objejto URLSearchParams e passou
+    // a coleta dos parametros da URL nele.
     let parametros = new URLSearchParams(window.location.search)
+    // Obteve do parametro "produto-id", o seu valor
     let parametroID = parametros.get("produto-id")
-    alert(parametroID)
-    //alert(window.location.search)// => ?produto-id=1
+
+    let indiceProd
+    for(let x in produtos){
+        console.log("o valor de x é: " + x)
+        console.log(`produtos[x].id é: ${produtos[x].id}`)
+        console.log(`parametroID é: ${parametroID}`)
+        console.log(`${produtos[x].id} == ${parametroID}`) 
+        if(produtos[x].id == parametroID){
+            //console.log("SIM")
+            indiceProd = x   
+        }
+    }
+
+    document.body.innerHTML = `
+        <img src="${produtos[indiceProd].img}" width="250" height"250">
+    `
+    //alert("indiceProd= "  + indiceProd)
+    //alert(parametroID)
+    //alert(window.location.search) // => ?produto-id=1
+
 }
 
 buscarDetalhes()
