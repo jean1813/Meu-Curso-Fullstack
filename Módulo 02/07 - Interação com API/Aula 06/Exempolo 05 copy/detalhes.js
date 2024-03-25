b74e187473f255a75acd5d7817ce005296fdf322
jean carlos <jeansousa.carlos@hotmail.com>
2024-03-25 17:27:38 -0300

    varias copias para refazer os codigos

--- Módulo 02/07 - Interação com API/Aula 06/Exempolo 05 copy/detalhes.js
+++ Módulo 02/07 - Interação com API/Aula 06/Exempolo 05 copy/detalhes.js
@@ -10,21 +10,32 @@ async function buscarDetalhes(){
     // Obteve do parametro "produto-id", o seu valor
     let parametroID = parametros.get("produto-id")
 
+    // Criou uma variável vazia
     let indiceProd
+    // Usou o for para percorrer toda a lista de produtos do JSON 
     for(let x in produtos){
         console.log("o valor de x é: " + x)
         console.log(`produtos[x].id é: ${produtos[x].id}`)
         console.log(`parametroID é: ${parametroID}`)
         console.log(`${produtos[x].id} == ${parametroID}`) 
         if(produtos[x].id == parametroID){
-            //console.log("SIM")
+            // Atribuiu à variável vazia, o valor de x, que contém
+            // o indice do produto que corresponde ao ID da URL
             indiceProd = x   
         }
     }
 
+    // Adiciona na TAG BODY do HTML, um código HTML concatenado
+    // com valores do objeto produto encontrado
     document.body.innerHTML = `
-        <h3>${produtos[indiceProd].nome}</h3>
+        <div class="detalhes-corpo">
         <img src="${produtos[indiceProd].img}" width="250" height"250">
+            <h3>${produtos[indiceProd].nome}</h3>
+            <p>${produtos[indiceProd].descricao}</p>
+            <span> O valor com desconto: R$ R$ ${produtos[indiceProd].valorComDesconto.toFixed(2).replace(".", ",") }</span>
+            <span class="color"> O valor sem desconto: R$  ${produtos[indiceProd].valorSemDesconto.toFixed(2).replace(".", ",")}</span>
+        </div>
+        
     `
     //alert("indiceProd= "  + indiceProd)
     //alert(parametroID)
