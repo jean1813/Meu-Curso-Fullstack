de1804b37b48b80732aebfefc2518250a7001116
jean carlos <jeansousa.carlos@hotmail.com>
2024-03-27 17:22:22 -0300

    acabou API

---
+++ Módulo 02/07 - Interação com API/Aula 06/Exempolo 06/detalhes.js
@@ -0,0 +1,53 @@
+async function buscarDetalhes(){  
+    let busca = await fetch("lista-produtos.json")
+    let produtos = await busca.json()
+    console.log(produtos)
+
+    let parametros = new URLSearchParams(window.location.search)
+    let parametroID = parametros.get("produto-id")
+
+    let indiceProd
+    for(let x in produtos){
+        console.log("o valor de x é: " + x)
+        console.log(`produtos[x].id é: ${produtos[x].id}`)
+        console.log(`parametroID é: ${parametroID}`)
+        console.log(`${produtos[x].id} == ${parametroID}`) 
+        if(produtos[x].id == parametroID){   
+            indiceProd = x   
+        }
+    }
+    document.getElementById("detalhes").innerHTML += ` 
+            <img src="${produtos[indiceProd].img[0]}" id="frame" width="250" height"250" style="border: 1px solid #000">
+            <div class="miniaturas" id="miniaturas"></div>
+            <h3>${produtos[indiceProd].nome}</h3>
+            <p>${produtos[indiceProd].descricao}</p>
+            <div class="valores">
+                <span> O valor com desconto: R$ ${produtos[indiceProd].valorComDesconto.toFixed(2).replace(".", ",") }</span>
+                <span> O valor sem desconto: R$ ${produtos[indiceProd].valorSemDesconto.toFixed(2).replace(".", ",")}</span>
+            </div>
+        `
+        let divMiniaturas = document.getElementById("miniaturas")
+        for(let y of produtos[indiceProd].img){
+            divMiniaturas.innerHTML +=`
+                <img src="${y}" class="mini" width="80" height=80" style="border: 1px solid #000">
+            `
+        }
+
+        let minizinhas = document.getElementsByClassName("mini")
+        for(let a of minizinhas){
+            a.addEventListener("mouseover", deslize)
+        }
+
+
+    //alert("indiceProd= "  + indiceProd)
+    //alert(parametroID)
+    //alert(window.location.search) // => ?produto-id=1
+
+}
+
+function deslize(){
+    document.getElementById("frame").src = this.src
+}
+
+buscarDetalhes()
+
---
+++ Módulo 02/07 - Interação com API/Aula 06/Exempolo 06/index.js
@@ -0,0 +1,68 @@
+// Função assíncrona para fazer o uso do Await
+async function buscas(){
+    // Await para(stop) a execução do programa aguardando
+    // o resultado do comando fetch()
+    let procura = await fetch("lista-produtos.json")//objeto resposta
+    let produtos = await procura.json()//lista do objeto resposta
+    let divLista = document.getElementById("lista-card")//retorna a tag html div.
+    //console.log(produtos)
+    //alert(procura)
+
+   // por que usar o for? => pra percorrer uma lista
+    // Quantas linhas de comando tem dentro do for? => 1 linha(divLista.inneHTML)
+    for(let produto of produtos){
+        divLista.innerHTML += `
+            <div class="card" data-id="${produto.id}">
+                <h3>${produto.nome}</h3>
+                <img src="${produto.img[0]} "width="200" height="200"/>
+                <p>${produto.descricao}</p>
+                <div class="valores">
+                    <span class="valorCom">R$ ${produto.valorComDesconto.toFixed(2).replace(".", ",")}</span>
+                    <span class="valorSem">R$ ${produto.valorSemDesconto.toFixed(2).replace(".",",")}</span>
+                </div>
+            </div>
+        `
+    }
+    // Buscar por todos os elementos HTML que contém "card"
+    // como valor do parâmetro "class".
+    let divsCards = document.getElementsByClassName("card") 
+    
+    for(let card of divsCards){
+     // Add em cada Div Card um evento que escuta quando o 
+    // usuário clica nele, e chama uma função.
+        card.addEventListener("click", clicou)          
+    }
+    
+}
+// É chamada essa função quando o usuário clicou 
+// em card que contém o evento de Escuta.
+function clicou(){
+    //alert("Jean Dev do Senai") => quando clicar em cima de cada card,
+    // Coleta o valor do atributo "data-id" do elemento
+    // HTML que acionou o evento de Escuta.
+    let elementoId = this.getAttribute("data-id")
+    //alert(window.location.href) printa o endereço atual da pagina.
+    //
+    window.location.href = "detalhes.html?produto-id=" + elementoId
+}
+
+buscas()
+
+
+    //in = ao indice da lista(0,1,2...)
+    //for(let produto in produtos){
+       // divLista.innerHTML += `
+            //<div class="card">
+
+                //<h3>${produtos[produto].nome}</h3>
+                //<img src="${produtos.[produto}" width="200" height="200"/>
+               // <p>${produtos[produto].descricao}</p>
+               // <p>${produto[produto].valorComDesconto}</p>
+               // <p>${produto[produto].valorSemDesconto}</p>
+
+            //</div>
+
+        //`
+    //}
+
+
