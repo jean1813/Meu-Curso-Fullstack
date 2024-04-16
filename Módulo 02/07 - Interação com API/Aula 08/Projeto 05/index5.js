917a5ad812b3c5738cb764a9af20b7d3acb05534
jean1813 <jeansousa.carlos@hotmail.com>
2024-04-16 17:22:48 -0300

    corrigindo erros, e criando mais códigos

--- Módulo 02/07 - Interação com API/Aula 08/Projeto 05/index5.js
+++ Módulo 02/07 - Interação com API/Aula 08/Projeto 05/index5.js
@@ -1,3 +1,9 @@
-async function{
-    
-}
+
+/*
+Clica no BOTÃO 
+  -> CHAMA A FUNÇÃO cliqueiNoBotao()
+  -> Vai no INPUT e pega o que está lá dentro
+
+  -> PASSAR a cidade para o servidor
+  Math.floor -> Ferramenta do JavaScript para Arredondar valores
+*/
